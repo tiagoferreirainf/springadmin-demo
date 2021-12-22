58ddfd3964b2bab8968665a272e1f19ac5a2d2e6
NB21971 <nb21971@novabase.pt>
2021-12-22 13:25:20 +0000

    added springadmin server microservice to project

---
+++ SpringAdminServer/springadminserver-micro/src/test/java/com/celfocus/springadmin/SpringadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celfocus.springadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
