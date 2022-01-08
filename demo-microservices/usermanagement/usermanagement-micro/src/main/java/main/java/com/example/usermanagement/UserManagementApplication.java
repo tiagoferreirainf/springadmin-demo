3d053c32809f0bbb112837c22a8f4078eab037b9
Tiago Ferreira <tiago.ferreira.inf@gmail.com>
2022-01-08 20:49:00 +0000

    - Update java and maven projects to support jdk to version 11 - Added eureka server - Update helm to version 3.7.2 - Added parent pom and moved dependency management - Added helm chart for usermanagement microservice

--- demo-microservices/usermanagement/usermanagement-micro/src/main/java/main/java/com/example/usermanagement/UserManagementApplication.java
+++ demo-microservices/usermanagement/usermanagement-micro/src/main/java/main/java/com/example/usermanagement/UserManagementApplication.java
@@ -2,7 +2,9 @@ package main.java.com.example.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserManagementApplication {
 
--- springadmin-server/springadminserver-micro/src/main/java/com/celfocus/springadmin/SpringadminApplication.java
+++ springadmin-server/springadminserver-micro/src/main/java/com/celfocus/springadmin/SpringadminApplication.java
@@ -4,11 +4,13 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringadminApplication {
 	public static void main(String[] args) {
