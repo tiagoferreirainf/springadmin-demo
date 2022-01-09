264f962d24b5df60d7caf7f3e9e3562f5591794c
Tiago Ferreira <tiago.ferreira.inf@gmail.com>
2022-01-09 00:10:10 +0000

    update usermanagement helm chart

--- springadmin-server/springadminserver-micro/src/main/java/com/celfocus/springadmin/SpringadminApplication.java
+++ springadmin-server/springadminserver-micro/src/main/java/com/celfocus/springadmin/SpringadminApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,5 +18,11 @@ import org.springframework.context.annotation.Configuration;
 public class SpringadminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringadminApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
